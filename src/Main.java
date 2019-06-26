a047987bedd87ba171f274e5126d8853f1c16123
Dariusz Dudek <darekdede1995@gmail.com>
2019-06-26 16:41:09 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,53 @@
 import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.net.URL;
 
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
         StringBuilder sb = new StringBuilder();
-
-        BufferedReader br = Files.newBufferedReader(Paths.get("C:\\Users\\user\\Desktop\\Pliki użytkownika darekdede1995 - Chomikuj.pl.htm"));
+        URL url = new URL("https://chomikuj.pl/" + args[0]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        //BufferedReader br = Files.newBufferedReader(Paths.get("C:\\Users\\user\\Desktop\\Pliki użytkownika darekdede1995 - Chomikuj.pl.htm"));
         {
-
-            // read line by line
+            boolean lista=false;
             String line;
-            while ((line = br.readLine()) != null) {
-                if (line.contains("<tbody><tr><td><div class=\"T_c\"><div id=\"Ti_0\" class=\"T_col\"></div></div></td><td><a href=\"https://chomikuj.pl/darekdede1995\" rel=\"0\" title=\"darekdede1995\" id=\"Ta_0\">")) {
-                    while (line.indexOf("title") != line.lastIndexOf("title")) {
-                        int pomocniczy;
-                        line = line.substring(line.indexOf("title="));
-                        line = line.substring(line.indexOf('"') + 1);
-                        pomocniczy = line.indexOf('"');
+            while ((line = br.readLine()) != null)
+            {
 
-                        if (!(line.substring(0, pomocniczy).contains("darekdede1995")) && !(line.substring(0, pomocniczy).contains("Zawiera nowe pliki")))
-                            sb.append(line.substring(0, pomocniczy)).append("\n");
+                if (lista)
+                {
+                    if(line.contains("foldersList"))
+                    {
+                        lista=true;
                     }
+                    continue;
                 }
-
+                if(line.contains("<a href=\"/" + args[0]) && !line.contains("folderOptionsTitle"))
+                {
+                    line=line.substring(0, line.lastIndexOf('"'));
+                    line=line.substring(line.lastIndexOf('"')+1);
+                    sb.append(line);
+                    sb.append("\n");
+                }
+                if (lista)
+                {
+                    if(line.contains("</table>"))
+                    {
+                        lista=false;
+                    }
+                    continue;
+                }
             }
+        }
+
             Writer w = new FileWriter("C:\\Users\\user\\Desktop\\opis.txt");
             w.write(sb.toString());
             w.close();
-
             System.out.println(sb);
-
-        }
-
-
     }
 }
+
